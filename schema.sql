b89d6b60ed81279ef52b902d2094e246edc94dde
cesarjakub <cesar@spsejecna.cz>
2023-12-29 12:09:56 +0100

    feat: mysql schema

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users(
+id int PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(20) not null,
+email VARCHAR(50) not null,
+password VARCHAR(255) not null
+);
+
+CREATE TABLE message(
+id int PRIMARY KEY AUTO_INCREMENT,
+SenderID INT not null,
+ReceiverID INT not null,
+MessageText VARCHAR(255) not null,
+Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (SenderID) REFERENCES users(id),
+FOREIGN KEY (ReceiverID) REFERENCES users(id)
+);
